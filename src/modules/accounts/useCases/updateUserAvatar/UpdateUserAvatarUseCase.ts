7fafa9bfbd97f8f1e31050e246aa4856be18e9e6
Felipe Castro <felipe.fcastro@mercadolivre.com>
2022-01-27 09:19:29 -0300

    adicionando inserção de avatar localmente

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,33 @@
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '../../../../errors/AppError';
+import { deleteFile } from '../../../../utils/file';
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+interface IRequest {
+    user_id: string;
+    avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private userRepository: UsersRepository
+    ) {}
+
+    async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+        const user = await this.userRepository.findById(user_id);
+
+        if (!user) {
+            throw new AppError('User not found!', 404);
+        }
+
+        if (user.avatar) await deleteFile(`./tmp/avatar/${user.avatar}`);
+        user.avatar = avatar_file;
+
+        await this.userRepository.create(user);
+    }
+}
+
+export { UpdateUserAvatarUseCase };
